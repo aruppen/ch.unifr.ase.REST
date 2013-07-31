b8f13c66f0fb658b4ff182232bcf5c237446b73e
Andreas Ruppen <andreas.ruppen@gmail.com>
2013-07-31 14:02:30 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CyberCoachServer2::Application.config.secret_key_base = '027bdb64fce60f405e4bcc668d5ac31814152cfa0217b4507efbae23b3420c392ae85b90856af0250060dc2225da91c91d2f96fcc000ac456c63a53a59ddb715'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CyberCoachServer2::Application.config.session_store :cookie_store, key: '_CyberCoachServer2_session'
